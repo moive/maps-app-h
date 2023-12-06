81482dc42d0c54342873493123e5d03e57d8efe1
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-12-06 11:24:49 -0500

    structure project

---
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './full-screen-page.component.html',
+  styleUrls: ['./full-screen-page.component.sass']
+})
+export class FullScreenPageComponent {
+
+}
---
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './markers-page.component.html',
+  styleUrls: ['./markers-page.component.sass']
+})
+export class MarkersPageComponent {
+
+}
