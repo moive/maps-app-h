9f872e18fb3c7df7104a093d4fa7c2328a0ebf50
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-12-12 13:38:07 -0500

    zoom level control

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -8,13 +8,43 @@ import { Map } from 'mapbox-gl';
 export class ZoomRangePageComponent implements AfterViewInit {
   @ViewChild('map') divMap?: ElementRef;
 
+  zoom: number = 10;
+  map?: Map;
+
   ngAfterViewInit(): void {
     if (!this.divMap) throw 'The element HTML is not found';
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap?.nativeElement, //'map', // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.zoom, // starting zoom
     });
+
+    this.mapListener();
+  }
+
+  mapListener() {
+    if (!this.map) throw 'The map is not initialized';
+    this.map.on('zoom', ev => {
+      this.zoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', ev => {
+      if (this.map!.getZoom() < 18) return;
+      this.map!.zoomTo(18);
+    });
+  }
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+
+  zoomChanged(value: string) {
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
   }
 }
