cb0afe285140f8c4ae36a8968747eb3ce7392eb9
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-12-12 11:43:46 -0500

    added mapbox

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,7 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
+(mapboxgl as any).accessToken =
+  'pk.eyJ1IjoibW9pdmUiLCJhIjoiY2xwdzRjc2dsMDlnazJqcW96a25ua3hmZCJ9.S1vZRXL4HperNuqbChZQ4w';
 @Component({
   templateUrl: './full-screen-page.component.html',
   styleUrls: ['./full-screen-page.component.scss'],
 })
-export class FullScreenPageComponent {}
+export class FullScreenPageComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
+}
