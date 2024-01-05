a85b30c2eda2839763371e3cd3238d07ecca16ef
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-01-05 00:39:42 -0500

    save marker in localStorage

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -5,6 +5,11 @@ interface MarkerAndColor {
   color: string;
   marker: Marker;
 }
+
+interface PlainMarker {
+  color: string;
+  lngLat: number[];
+}
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrls: ['./markers-page.component.scss'],
@@ -27,6 +32,8 @@ export class MarkersPageComponent {
       center: this.currentLngLat, // starting position [lng, lat]
       zoom: this.zoom, // starting zoom
     });
+
+    this.readFromLocalStorage();
 
     /* const markerHtml = document.createElement('div');
     markerHtml.innerHTML = '😀';
@@ -60,17 +67,43 @@ export class MarkersPageComponent {
       .addTo(this.map);
 
     this.markers.push({ color, marker });
+    this.saveToLocalStorage();
+
+    marker.on('dragend', () => this.saveToLocalStorage());
   }
 
   deleteMarker(index: number): void {
     this.markers[index].marker.remove();
     this.markers.splice(index, 1);
+    this.saveToLocalStorage();
   }
 
   flyTo(marker: Marker) {
     this.map?.flyTo({
       zoom: 14,
       center: marker.getLngLat(),
+    });
+  }
+
+  saveToLocalStorage() {
+    const plainMarkers: PlainMarker[] = this.markers.map(
+      ({ color, marker }) => ({
+        color,
+        lngLat: marker.getLngLat().toArray(),
+      })
+    );
+
+    localStorage.setItem('plainMarkers', JSON.stringify(plainMarkers));
+  }
+
+  readFromLocalStorage() {
+    const plainMarkersString = localStorage.getItem('plainMarkers') ?? '[]';
+    const plainMarkers: PlainMarker[] = JSON.parse(plainMarkersString);
+
+    plainMarkers.forEach(({ color, lngLat }) => {
+      const [lng, lat] = lngLat;
+      const coords = new LngLat(lng, lat);
+      this.addMarker(coords, color);
     });
   }
 }
