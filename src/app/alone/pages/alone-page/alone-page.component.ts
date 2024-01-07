228b7334aee3fbd2e591ddf6e1976655bcfbe427
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-01-06 21:10:56 -0500

    standalone as a page

---
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './alone-page.component.html',
+  styleUrls: ['./alone-page.component.scss'],
+})
+export class AlonePageComponent {}
